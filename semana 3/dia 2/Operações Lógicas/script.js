af1eefc807d2f4f36d04c23c74fd6ecd7064569e
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-10-17 11:51:17 -0300

    dia 2

---
+++ semana 3/dia 2/Operações Lógicas/script.js
@@ -0,0 +1,8 @@
+let spaceship = "Golias"
+let velocity = 80
+console.log(velocity === "80")
+console.log(velocity == "80")
+console.log(velocity != "80")
+console.log(velocity !== "80")
+console.log(velocity > "70")
+console.log(velocity >= "200")
