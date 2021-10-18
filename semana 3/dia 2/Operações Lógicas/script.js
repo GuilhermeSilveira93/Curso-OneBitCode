b386730fa20a7356c21a04a6642b37370040e433
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-10-18 17:04:48 -0300

    dia 2

--- semana 3/dia 2/Operações Lógicas/script.js
+++ semana 3/dia 2/Operações Lógicas/script.js
@@ -1,8 +1,24 @@
-let spaceship = "Golias"
-let velocity = 80
-console.log(velocity === "80")
-console.log(velocity == "80")
-console.log(velocity != "80")
-console.log(velocity !== "80")
-console.log(velocity > "70")
-console.log(velocity >= "200")
+/*--------------------- E (&&) -----------------------*/
+let spaceship = "Elemental"
+let velocity = 20
+
+console.log(spaceship.length == 9 && velocity > 15)
+// V e V = V
+// SÓ SERÁ TRUE, se todas as comparações forem verdadeiras.
+/*--------------------- E (&&) -----------------------*/
+
+/*--------------------- OU (||) -----------------------*/
+let ship = "Elemental"
+let velo = 20
+
+console.log(ship.length == 11 || velo > 15)
+// SERÁ TRUE, pelo menos 1 das comparações forem verdadeiras.
+/*--------------------- OU (||) -----------------------*/
+
+/*--------------------- NÃO (||) -----------------------*/
+let shi = "Elemental"
+let vel = 20
+
+console.log(!(vel > 19))
+// NÃO V = F
+//O NÃO INVERTE O RESULTADO, SE FOR FALSO SERÁ APRESENTADO VERDADEIRO
