f4a203984241800435d5395e217ee6ddede55a7f
Guilherme Padovani Silveira <guilherme.padovani93@gmail.com>
2021-12-14 19:39:54 -0300

    funções basicas de array
    aprendemos funções como:
    Pop - apaga ultimo elemento
    shift - apaga primeiro elemento
    unshift - adiciona um novo elemento no começo do array
    length - retorna o tamanho

--- semana 3/dia 5/Introducao aos arrays/index.js
+++ semana 3/dia 5/Introducao aos arrays/index.js
@@ -5,3 +5,6 @@ console.log(hitchedSpaceship[2])
 let spaceship = new Array(1,2,3,4,"Olá")
 
 console.log(spaceship)
+
+let getNumber = new Array(5)
+console.log(getNumber)
---
+++ semana 3/dia 5/funções basicas de array/funcoesarray.js
@@ -0,0 +1,16 @@
+let naveEspacial = ['SpaceX','Milenium Falcon']
+console.log(naveEspacial)
+
+naveEspacial.push('Obzidian')/*Adiciona um Elemento no final*/
+console.log(naveEspacial)
+
+naveEspacial.pop()/*Apaga Ultimo Elemento*/
+console.log(naveEspacial)
+
+naveEspacial.shift()/*Apaga o Primeiro Elemento*/
+console.log(naveEspacial)
+
+naveEspacial.unshift('Fenix')/*Adiciona um novo elemento no começo*/
+console.log(naveEspacial)
+
+console.log(naveEspacial.length)/*Apaga o Primeiro Elemento*/
