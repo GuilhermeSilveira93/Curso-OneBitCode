e08b3d3726cb9f08f4270ba5e0e07db518a687fc
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-03-01 10:50:11 -0300

    identacao dos codigos
    da semana 1 até a semana 2.
    Todos os dias de aula.

--- semana 3/dia 1/Declarando Variáveis/script.js
+++ semana 3/dia 1/Declarando Variáveis/script.js
@@ -3,22 +3,22 @@ let spaceship = "FE Supernova "
 console.log(spaceship)
 spaceship = "FE Helmet"
 console.log(spaceship)
-/*---------------------------------*/
+/*---------------------------------
 const equipe = "Estelar"
 console.log(equipe)
-/*equipe = "Espacial"
-console.log(equipe)*/
-/*---------------------------------*/
+equipe = "Espacial"
+console.log(equipe)
+/*---------------------------------
 var velocidade = 90
 console.log(velocidade)
 velocidade = 25
 console.log(velocidade)
-/*---------------------------------*/
+/*---------------------------------
 let departureConfirmation = confirm("Nave Pronta para decolar. Aguardando Confirmação !")
 console.log(departureConfirmation)
-/*---------------------------------*/
+/*---------------------------------
 let pilot = prompt('Qual seu nome, piloto ?')
 console.log(pilot)
 /*---------------------------------*/
 /*CAMEL CASE*/
-myAge
+/*myAge*/
