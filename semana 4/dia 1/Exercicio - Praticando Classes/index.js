146920d9c46359e7db6a9303ea4a76f57c6760da
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-03-10 13:02:23 -0300

    Update index.js

--- semana 4/dia 1/Exercicio - Praticando Classes/index.js
+++ semana 4/dia 1/Exercicio - Praticando Classes/index.js
@@ -11,10 +11,25 @@ class NaveEspacial {
   }
 }
 
+let spaceList = []
+
+function cadastro(){
+ let nome = prompt(`Qual o nome da sua nave?`)
+ let quantidadeTripulantes = prompt(`Quantos tripulantes ela tem?`)
+ new NaveEspacial (nome, quantidadeTripulantes, true , true)/* NEW FAZ OS DADOS ENTRAREM NO CONSTRUTOR DA CLASSE*/
+ spaceList.push(nome + " " + quantidadeTripulantes)/*ADICIONA DADOS AO ARRAY*/
+}
+
+function imprime(){
+ let imprimenave =  spaceList.join(`\n`)/*INCLUIU A QUEBRA DE LINHAS ENTRE AS VARIAVEIS / JOIN*/
+  alert(`${imprimenave}`)
+}
+
 function escolha() {
+  let opcao
   do {
-    let choseoption = Number(prompt("MENU DA ESTAÇÃO\n1 - Cadastrar Nave\n2 - Imprimir Dados\n3 - Sair do programa"))
-    switch (choseoption) {
+    opcao = Number(prompt("MENU DA ESTAÇÃO\n1 - Cadastrar Nave\n2 - Imprimir Dados\n3 - Sair do programa"))
+    switch (opcao) {
       case 1:
         cadastro()
         break;
@@ -26,66 +41,6 @@ function escolha() {
       default:
         alert("Escolha uma opção válida!")
     }
-  } while (choseoption != 3);
+  } while (opcao != 3);
 }
 escolha()
-
-function cadastro() {
-  nome = prompt("ENGATE DE NAVE\nDIGITE O NOME DA NAVE.")
-  tripulantes = prompt("ENGATE DE NAVE\nDIGITE O NÚMERO DE TRIPULANTES.")
-  portas = 'fechadas'
-  engate = true
-}
-
-function imprime() {
-  alert(NaveEspacial)
-}
-
-/*------------------------------------------------------------------------------------*/
-
-
-class spaceShip{
-  constructor(name, crewQuantity){
-      this.name = name;
-      this.crewQuantity= crewQuantity;
-      this.doors = false;
-      this.engate = false;
-  };
-  engatetrue(){
-      this.engate = true;
-      this.doors = true;
-  }
-}
-let spaceList = []
-
-function createShip(){
- let newShip = prompt(`Qual o nome da sua nave?`)
- let newCrewQuantity = prompt(`Quantos tripulantes ela tem?`)
- new spaceShip (newShip, newCrewQuantity, true , true)
- spaceList.push(newShip + " " + newCrewQuantity)
-}
-
-function printShip(){
- let printShip =  spaceList.join(`\n`)
-  alert(`${printShip}`)
-}
-
-function showMenu(){
-  let chosenOptions
-  do {
-      chosenOptions = prompt(`Digite a opção desejea \n1- Cadastrar nova spaceship\n2- imprimir lista de spaceships\n3-sair do programa`)
-      switch(chosenOptions){
-          case "1":
-              createShip()
-              break;
-          case "2":
-              printShip()
-              break;
-          case "3":
-              break;
-          default:
-              alert("escolha uma opção valida")
-      }
-  } while (chosenOptions != "3");
-}
-showMenu()
