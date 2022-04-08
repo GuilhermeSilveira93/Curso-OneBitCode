bfb0f753afef5bd56ede73831cda4011c0f43450
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-04-08 11:30:04 -0300

    Update index.js

--- semana 4/dia 1/Exercicio - Praticando Classes/index.js
+++ semana 4/dia 1/Exercicio - Praticando Classes/index.js
@@ -1,30 +1,3 @@
-class NaveEspacial {
-  constructor(nome, tripulantes) {
-    this.nome = nome
-    this.tripulantes = tripulantes
-    this.portas = false
-    this.engate = false
-  }
-  engate(){
-    this.portas = true
-    this.engate = true
-  }
-}
-
-let listanave = []
-
-function cadastro(){
- let nome = prompt(`Qual o nome da sua nave?`)
- let quantidadeTripulantes = prompt(`Quantos tripulantes ela tem?`)
- new NaveEspacial (nome, quantidadeTripulantes, true , true)/* NEW FAZ OS DADOS ENTRAREM NO CONSTRUTOR DA CLASSE*/
- listanave.push(nome + " " + quantidadeTripulantes)/*ADICIONA DADOS AO ARRAY*/
-}
-
-function imprime(){
- let imprimenave =  listanave.join(`\n`)/*INCLUIU A QUEBRA DE LINHAS ENTRE AS VARIAVEIS / JOIN*/
-  alert(`${imprimenave}`)
-}
-
 function escolha() {
   let opcao
   do {
@@ -43,4 +16,37 @@ function escolha() {
     }
   } while (opcao != 3);
 }
+
+let listanave = []/*ARRAY VAZIO o texto*/
+
+function cadastro(){
+  let nome = prompt(`Qual o nome da sua nave?`)
+  let quantidadeTripulantes = prompt(`Quantos tripulantes ela tem?`)
+ new NaveEspacial (nome, quantidadeTripulantes, true , true)/* NEW FAZ OS DADOS ENTRAREM NO CONSTRUTOR DA CLASSE*/
+ listanave.push('Nome: ' +nome + " " +' Tripulantes: ' + quantidadeTripulantes)/*ADICIONA DADOS AO ARRAY*/
+}
+
+function imprime(){
+  let imprimenave =  listanave.join(`\n`)/*INCLUIU A QUEBRA DE LINHAS ENTRE AS VARIAVEIS / JOIN*/
+  alert(`${imprimenave}`)
+}
+
+class NaveEspacial {
+  constructor(nome, tripulantes) {
+    this.nome = nome
+    this.tripulantes = tripulantes
+    this.portas = false
+    this.engate = false
+  }
+  engate(){
+    this.portas = true
+    this.engate = true
+  }
+}
 escolha()
+
+/*NESSA ORDEM, FICOU MELHOR DE ENTENDER.
+1º COMEÇAMOS COM O MENU
+2º DEFINIMOS AS FUNÇÕES DE CADA OPÇÃO
+3º ARMAZENAMOS TUDO EM OBJETOS.
+*/
