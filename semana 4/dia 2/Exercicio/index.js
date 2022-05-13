6a724df1c29e74993d9beaa60c26dbae0c37c4b3
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-05-13 16:06:19 -0300

    Update index.js

--- semana 4/dia 2/Exercicio/index.js
+++ semana 4/dia 2/Exercicio/index.js
@@ -3,6 +3,31 @@ Vamos criar um sistema de monitoramento de uma nave espacial.
 Logo que o sistema é iniciado, ele deve pedir os dados da nave.
 Para a nave, que for cadastrada, precisamos de:  *Nome
                                                                               *Quantidade de Tripulantes
+*/
+class NaveEspacial{
+    constructor(nome,quantidade_tripulantes){
+    this.nome = nome
+    this.quantidade_tripulantes = quantidade_tripulantes
+    }
+}
+class Nave_Batalha{
+    constructor(nome,quantidade_tripulantes,quantidade_armas){
+    super(nome,quantidade_tripulantes)
+    this.quantidade_armas = quantidade_armas
+}
+}
+class Nave_Transporte{
+    constructor(nome,quantidade_tripulantes,quantidade_assentos){
+    super(nome,quantidade_tripulantes)
+    this.quantidade_assentos = quantidade_assentos
+}
+}
+
+let nome_nave = prompt("Digite o nome da Nave.")
+let quantidade_tripulantes = prompt("Digite a quantidade de Tripulantes.")
+let velocidade_atual = 0
+
+/*
 --------------------------------------------------------------------------------------------------------------------------------
 Os veículos podem ser de batalha ou de transporte.
 Caso seja de batalha: ele também deverá pedir quantas armas a nave tem disponíveis.
