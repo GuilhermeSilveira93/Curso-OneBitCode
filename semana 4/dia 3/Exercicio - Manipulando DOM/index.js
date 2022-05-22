78aacf23092df2f0cbd15a911140969102b3d00f
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-05-22 15:21:38 -0300

    1.0

--- semana 4/dia 3/Exercicio - Manipulando DOM/index.js
+++ semana 4/dia 3/Exercicio - Manipulando DOM/index.js
@@ -1,7 +1,27 @@
-function adicionar(){
-  let casa = document.querySelectorAll('form#formulariocasa')
-  let clonador = casa.cloneNode(true)
-  let contador = casa.children.length + 1
+function Listar(){
+  let bairro = document.querySelector("input[name='bairro']").value/*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
+  let cidade = document.querySelector("input[name='cidade']").value/*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
+  let area = document.querySelector("input[name='area']").value/*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
+  let numero = document.querySelector("input[name='numero']").value/*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
 
-  document.body.appendChild(clonador)
+  /*CRIANDO LISTA NOVA QUE VAI MOSTRAR OS DADOS RECEBIDOS*/
+  let lista = document.createElement("li")
+  lista.innerText = area + ' mt², número :' + numero + ", Bairro: " + bairro + ", Cidade: " + cidade;
+
+  /*CRIANDO BOTÃO REMOVER*/
+  let botaoremover = document.createElement("button")
+  botaoremover.type = "button"
+  botaoremover.innerText = "Remover"
+  botaoremover.setAttribute("onclick", "remover(this)")
+
+  /*COLOCANDO O BOTAO DENTRO DA LISTA NOVA*/
+  lista.appendChild(botaoremover)
+
+  /*COLOCANDO TUDO NO DOM*/
+  document.getElementById("listadinamica").appendChild(lista)
+}
+
+function remover(button){
+  let listapararemover = button.parentNode
+  document.getElementById("listadinamica").removeChild(listapararemover)
 }
