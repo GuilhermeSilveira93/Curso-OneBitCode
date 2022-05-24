bf8a94a92b916f0c6c834674ac76ab8c0933dde3
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-05-24 19:56:05 -0300

    1.0

--- semana 4/dia 3/Exercicio - Manipulando DOM/index.js
+++ semana 4/dia 3/Exercicio - Manipulando DOM/index.js
@@ -3,10 +3,9 @@ function Listar(){
   let cidade = document.querySelector("input[name='cidade']").value/*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
   let area = document.querySelector("input[name='area']").value/*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
   let numero = document.querySelector("input[name='numero']").value/*RECEBENDO DADOS E COLOCANDO NA VARIAVEL*/
-
   /*CRIANDO LISTA NOVA QUE VAI MOSTRAR OS DADOS RECEBIDOS*/
   let lista = document.createElement("li")
-  lista.innerText = area + ' mt², número :' + numero + ", Bairro: " + bairro + ", Cidade: " + cidade;
+  lista.innerText = area + 'mt² \nnúmero :' + numero + "\nBairro: " + bairro + ", \nCidade: " + cidade;
 
   /*CRIANDO BOTÃO REMOVER*/
   let botaoremover = document.createElement("button")
