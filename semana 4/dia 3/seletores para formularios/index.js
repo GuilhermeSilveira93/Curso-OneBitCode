797689555d34f310172c20ed2c6e283ccaaef51d
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-06-06 15:39:33 -0300

    Update index.js

--- semana 4/dia 3/seletores para formularios/index.js
+++ semana 4/dia 3/seletores para formularios/index.js
@@ -5,7 +5,7 @@ function showInfo(){
   /*PEGANDO VARIOS VALORES DE INPUT */
   let developerOptions = document.querySelectorAll("input[name='developer-options']:checked")
   let optionValue = []/*vai receber todos os dados*/
-  developerOptions.forEach(e => {optionValue.push(e.value)}) 
+  developerOptions.forEach(e => {optionValue.push(e.value)}) /*vai percorrer o array, adicionando cada elemento nele*/
   let optionsChecked = optionValue.join(", ")
   alert("Nome: " + name + "\nCor Primaria: " + favoriteColor + "\nGosta de Programar ? " + likeprogramin + "\nConhecimentos em programação: " + optionsChecked)
 }
