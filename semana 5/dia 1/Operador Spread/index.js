ededb38ed5fe6fd40956bc427113afab092f58ed
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-06-16 16:31:14 -0300

    Update index.js

--- semana 5/dia 1/Operador Spread/index.js
+++ semana 5/dia 1/Operador Spread/index.js
@@ -2,23 +2,17 @@ let spaceship =[ 'Colossus',
                           'Artemis',
                           'Fenix'
 ]
-/*
+
 console.log(...spaceship)
-console.log(spaceship)*/
+console.log(spaceship)
 
-class carro{
-  constructor(marca,potencia,peso,portas){
-  this.marca = marca
-  this.potencia = potencia
-  this.peso = peso
-  this.portas = portas
-  }
+
+function equipe(message, names) {
+  names.forEach(name => console.log(`${message}${name}`))
 }
+equipe("Seja Bem-vindo", [" Arthur"," Jake"," Melissa"])
 
-let carro1 = new carro('Fiat','74cv','800kg',4)
-
-console.log(carro1)
-
-array.forEach(carro1 => {
-  
-});
+function equipe2(message, ...names) {
+  names.forEach(name => console.log(`${message}${name}`))
+}
+equipe2("Adeus, ", " Pedro"," Beatriz"," Layane")
