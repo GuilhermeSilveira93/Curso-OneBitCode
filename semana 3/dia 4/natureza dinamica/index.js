137b948de75da15f2b6f81b8f89f47145c2ab995
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-06-22 16:38:09 -0300

    Update index.js

---
+++ semana 3/dia 4/natureza dinamica/index.js
@@ -0,0 +1,14 @@
+class planeta{
+  constructor(nome,constelacao,tamanho,estrelas){
+    this.nome = nome
+    this.constelacao = constelacao
+    this.tamanho = tamanho
+    this.estrelas = estrelas
+  }
+}
+class solado extends planeta{
+  this.fire = 'Fogo'
+}
+let saturno = new sol('saturno','Andromeda','200Bilhões de Km',3,true)
+console.log(saturno)
+/*---------------------------------------------------------------------------*/
