fd0778c35b2ccaad3d482d8e5c850e7293bd8490
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-07-08 16:09:32 -0300

    1

--- semana 5/dia 1/Transpilação com Babel/index.js
+++ semana 5/dia 1/Transpilação com Babel/index.js
@@ -1,3 +1,13 @@
 function testingES8(text1,text2,){
   alert(`${text1}\n${text2}`)
 }
+
+/*INSTALAÇÃO*/
+/*npm install @babel/core @babel/cli @babel/preset-env --save-dev*/
+
+
+
+/*npx babel 'nome do arquivo'*/
+
+/*Salvando*/
+/*npx babel 'nome do arquivo' -o 'nome do novo arquivo'*/
---
+++ semana 5/dia 1/Transpilação com Babel/index2.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function testingES8(text1, text2) {
+  alert("".concat(text1, "\n").concat(text2));
+}
+/*INSTALAÇÃO*/
+
+/*NPM INSTALL @BABEL/CORE @BABEL/CLI @BABEL/PRESET-ENV --SAVE-DEV
+colocar tudo em minusculo.*/
+
+/*npx balel 'nome do arquivo'*/
+
+/*Salvando*/
+
+/*npx babel 'nome do arquivo' -o 'nome do novo arquivo'*/
