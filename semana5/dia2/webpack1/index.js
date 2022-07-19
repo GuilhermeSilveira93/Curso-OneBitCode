c3b19b9152b41dd697ebe48ba6ef214db206ac95
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2022-07-19 12:23:25 -0300

    Update index.js

--- semana5/dia2/webpack1/index.js
+++ semana5/dia2/webpack1/index.js
@@ -1,4 +1,4 @@
 /*INSTALANDO WEBPACK*/
 /* 1º PASSO: no CMD, depois de dar um npm init
               sudo chown -R $usar:$GROUP ~/.npm
-              sudo chown -R $usar:$GROUP ~/.config
+              sudo chown -R $usar:$GROUP ~/.config*/
